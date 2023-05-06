e037b3fd09ceb6b5cb95eff85d89bf33a07203c2
Ryan <106937234+optimistic-ryan@users.noreply.github.com>
2023-05-06 18:35:41 -0400

    Update Options Trading Simulator.sql

--- Options Trading Simulator.sql
+++ Options Trading Simulator.sql
@@ -4,7 +4,7 @@ CREATE TABLE underlying_assets (
   id INT PRIMARY KEY,
   current_price DECIMAL(10, 2) NOT NULL,
   ticker_symbol VARCHAR(10) UNIQUE NOT NULL,
-  company_name VARCHAR(100) NOT NULL
+  company_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE options_contracts (
@@ -20,7 +20,6 @@ CREATE TABLE options_contracts (
 );
 
 CREATE TABLE trades (
-  id INT PRIMARY KEY,
   options_contract_id INT NOT NULL,
   trade_type CHAR(4) NOT NULL,
   quantity INT NOT NULL,
@@ -29,52 +28,54 @@ CREATE TABLE trades (
   FOREIGN KEY (options_contract_id) REFERENCES options_contracts(id)
 );
 
+
 CREATE TABLE trading_strategies (
   id INT PRIMARY KEY,
-  strategy_name VARCHAR(50) NOT NULL
+  strategy_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE strategy_trades (
   strategy_id INT NOT NULL,
-  trade_id INT NOT NULL,
-  PRIMARY KEY (strategy_id, trade_id),
+  options_contract_id INT NOT NULL,
+  trade_type CHAR(4) NOT NULL,
+  trade_date DATE NOT NULL,
+  PRIMARY KEY (strategy_id, options_contract_id, trade_type, trade_date),
   FOREIGN KEY (strategy_id) REFERENCES trading_strategies(id),
-  FOREIGN KEY (trade_id) REFERENCES trades(id)
+  FOREIGN KEY (options_contract_id, trade_type, trade_date) REFERENCES trades(options_contract_id, trade_type, trade_date)
 );
-
 
 -- Insert Data into Tables
 
 INSERT INTO underlying_assets (id, current_price, ticker_symbol, company_name) VALUES
-(1, 150, 'AAPL', 'Apple Inc.'),
-(2, 250, 'GOOGL', 'Alphabet Inc.'),
-(3, 100, 'MSFT', 'Microsoft Corporation'),
-(4, 300, 'AMZN', 'Amazon.com Inc.'),
-(5, 200, 'TSLA', 'Tesla Inc.');
+(1, 150.00, 'AAPL', 'Apple Inc.'),
+(2, 250.00, 'GOOGL', 'Alphabet Inc.'),
+(3, 100.00, 'MSFT', 'Microsoft Corporation'),
+(4, 300.00, 'AMZN', 'Amazon.com Inc.'),
+(5, 200.00, 'TSLA', 'Tesla Inc.');
 
 INSERT INTO options_contracts (id, underlying_asset_id, option_type, strike_price, expiration_date, risk_free_rate, volatility, dividend_yield) VALUES
-(1, 1, 'CALL', 150.00, '2022-05-19', 0.02, 0.5, 0.2),
-(2, 1, 'PUT', 150.00, '2022-05-19', 0.02, 0.5, 0.2),
-(3, 2, 'CALL', 1200.00, '2022-05-19', 0.02, 0.5, 0.2),
-(4, 2, 'PUT', 1200.00, '2022-05-19', 0.02, 0.5, 0.2),
-(5, 3, 'CALL', 200.00, '2022-05-19', 0.02, 0.5, 0.2),
-(6, 3, 'PUT', 200.00, '2022-05-19', 0.02, 0.5, 0.2),
-(7, 4, 'CALL', 3300.00, '2022-05-19', 0.02, 0.5, 0.2),
-(8, 4, 'PUT', 3300.00, '2022-05-19', 0.02, 0.5, 0.2),
-(9, 5, 'CALL', 600.00, '2022-05-19', 0.02, 0.5, 0.2),
-(10, 5, 'PUT', 600.00, '2022-05-19', 0.02, 0.5, 0.2);
+(1, 1, 'CALL', 150.00, '2022-05-19', 0.02, 0.50, 0.20),
+(2, 1, 'PUT', 150.00, '2022-05-19', 0.02, 0.50, 0.20),
+(3, 2, 'CALL', 1200.00, '2022-05-19', 0.02, 0.50, 0.20),
+(4, 2, 'PUT', 1200.00, '2022-05-19', 0.02, 0.50, 0.20),
+(5, 3, 'CALL', 200.00, '2022-05-19', 0.02, 0.50, 0.20),
+(6, 3, 'PUT', 200.00, '2022-05-19', 0.02, 0.50, 0.20),
+(7, 4, 'CALL', 3300.00, '2022-05-19', 0.02, 0.50, 0.20),
+(8, 4, 'PUT', 3300.00, '2022-05-19', 0.02, 0.50, 0.20),
+(9, 5, 'CALL', 600.00, '2022-05-19', 0.02, 0.50, 0.20),
+(10, 5, 'PUT', 600.00, '2022-05-19', 0.02, 0.50, 0.20);
 
-INSERT INTO trades (id, options_contract_id, trade_type, quantity, trade_date, trade_price) VALUES
-(1, 1, 'BUY', 10, '2022-05-01', 5.00),
-(2, 2, 'SELL', 10, '2022-05-01', 4.00),
-(3, 3, 'BUY', 5, '2022-05-02', 10.00),
-(4, 4, 'SELL', 5, '2022-05-02', 8.00),
-(5, 5, 'BUY', 15, '2022-05-03', 7.00),
-(6, 6, 'SELL', 15, '2022-05-03', 6.00),
-(7, 7, 'BUY', 4, '2022-05-04', 25.00),
-(8, 8, 'SELL', 4, '2022-05-04', 20.00),
-(9, 9, 'BUY', 8, '2022-05-05', 12.00),
-(10, 10, 'SELL', 8, '2022-05-05', 10.00);
+INSERT INTO trades (options_contract_id, trade_type, quantity, trade_date, trade_price) VALUES
+(1, 'BUY', 10, '2022-05-01', 5.00),
+(2, 'SELL', 10, '2022-05-01', 4.00),
+(3, 'BUY', 5, '2022-05-02', 10.00),
+(4, 'SELL', 5, '2022-05-02', 8.00),
+(5, 'BUY', 15, '2022-05-03', 7.00),
+(6, 'SELL', 15, '2022-05-03', 6.00),
+(7, 'BUY', 4, '2022-05-04', 25.00),
+(8, 'SELL', 4, '2022-05-04', 20.00),
+(9, 'BUY', 8, '2022-05-05', 12.00),
+(10, 'SELL', 8, '2022-05-05', 10.00);
 
 INSERT INTO trading_strategies (id, strategy_name) VALUES
 (1, 'Bullish Call Spread'),
@@ -83,17 +84,18 @@ INSERT INTO trading_strategies (id, strategy_name) VALUES
 (4, 'Butterfly Spread'),
 (5, 'Straddle');
 
-INSERT INTO strategy_trades (strategy_id, trade_id) VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 4),
-(3, 5),
-(3, 6),
-(4, 7),
-(4, 8),
-(5, 9),
-(5, 10);
+INSERT INTO strategy_trades (strategy_id, options_contract_id, trade_type, trade_date) VALUES
+(1, 1, 'BUY', '2022-05-01'),
+(1, 2, 'SELL', '2022-05-01'),
+(2, 3, 'BUY', '2022-05-02'),
+(2, 4, 'SELL', '2022-05-02'),
+(3, 5, 'BUY', '2022-05-03'),
+(3, 6, 'SELL', '2022-05-03'),
+(4, 7, 'BUY', '2022-05-04'),
+(4, 8, 'SELL', '2022-05-04'),
+(5, 9, 'BUY', '2022-05-05'),
+(5, 10, 'SELL', '2022-05-05');
+
 
 
 -- Query the Data
@@ -123,7 +125,7 @@ BEGIN
   RETURN QUERY
   SELECT t.*
   FROM trades t
-  JOIN strategy_trades st ON t.id = st.trade_id
+  JOIN strategy_trades st ON t.options_contract_id = st.options_contract_id AND t.trade_type = st.trade_type AND t.trade_date = st.trade_date
   WHERE st.strategy_id = p_strategy_id;
 END;
 $$ LANGUAGE plpgsql;
@@ -143,7 +145,7 @@ BEGIN
   RETURN QUERY
   SELECT st.strategy_id, SUM(t.trade_price * t.quantity * (CASE t.trade_type WHEN 'BUY' THEN -1 ELSE 1 END)) as net_cost
   FROM trades t
-  JOIN strategy_trades st ON t.id = st.trade_id
+  JOIN strategy_trades st ON t.options_contract_id = st.options_contract_id AND t.trade_type = st.trade_type
   WHERE st.strategy_id = p_strategy_id
   GROUP BY st.strategy_id;
 END;
@@ -159,7 +161,7 @@ BEGIN
              (CASE oc.option_type WHEN 'CALL' THEN (GREATEST(0, ua.current_price - oc.strike_price)) ELSE (GREATEST(0, oc.strike_price - ua.current_price)) END)
              ) as profit_loss
   FROM trades t
-  JOIN strategy_trades st ON t.id = st.trade_id
+  JOIN strategy_trades st ON t.options_contract_id = st.options_contract_id AND t.trade_type = st.trade_type
   JOIN options_contracts oc ON t.options_contract_id = oc.id
   JOIN underlying_assets ua ON oc.underlying_asset_id = ua.id
   WHERE st.strategy_id = p_strategy_id
